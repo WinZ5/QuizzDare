6e8baf75c9f93924318e44b9311e51c9ac7a1d76
SP.Win <supawit.promma6256@gmail.com>
2024-02-23 01:49:18 +0700

    fix scoreSorter error

--- util/Util.java
+++ util/Util.java
@@ -15,7 +15,7 @@ public class Util {
 
         for (int i = 0; i < list.size() - 1; i++) {
             for (int j = 0; j < list.size()  - 1 - i; j++) {
-                if (list.get(j).getScore() > list.get(j + 1).getScore()) {
+                if (list.get(j).getScore() < list.get(j + 1).getScore()) {
                     User temp = list.get(j);
                     list.set(j, list.get(j + 1));
                     list.set(j + 1, temp);
